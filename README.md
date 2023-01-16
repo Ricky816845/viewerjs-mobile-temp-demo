94c681f601d13736162406baf1db640c4d59d65c
liuxiang <liuxiang@dachkj.com>
2023-01-16 23:29:45 +0800

    init

---
+++ README.md
@@ -0,0 +1,18 @@
+# 复现步骤
+
+- Project setup
+```
+yarn 
+```
+
+- start development server
+```
+yarn start
+```
+- 在手机端浏览器打开页面
+- 点击 预览 然后再点击周围关闭
+
+
+# Note
+
+#### patches文件是我的临时处理 将文件后缀.exclude解除再运行yarn即可生效
---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-app class="app">
+    <v-main>
+      <v-container>
+        <v-card>
+          <v-card-text>
+            <h1>Test</h1>
+            <img src="../src/assets/demopic.jpg" @click="viewImage($event)" />
+          </v-card-text>
+        </v-card>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+<style>
+img {
+  width: 100%;
+}
+</style>
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      opitons: {
+        button: false,
+        navbar: false,
+        title: false,
+        toolbar: false,
+        tooltip: false,
+      },
+    };
+  },
+  methods: {
+    viewImage(event) {
+      this.$viewerApi({
+        options: this.opitons,
+        images: [event.target.src],
+      });
+    },
+  },
+};
+</script>
